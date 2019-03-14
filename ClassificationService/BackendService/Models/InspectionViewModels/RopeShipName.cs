cc2c6d3e127cb497df8250b668745349640ac590
riccardodl <riccardodl92@gmail.com>
2019-03-14 07:45:01 +0100

    experiment with selection and dropdown

---
+++ ClassificationService/BackendService/Models/InspectionViewModels/RopeShipName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendService.Models.InspectionViewModels
+{
+    public class RopeShipName
+    {
+        public string ShipName { get; set; }
+        public int RopeID { get; set; }
+        public int ShipID { get; set; }
+        public bool Selected { get; set; }
+    }
+}
---
+++ ClassificationService/BackendService/Pages/Images/RopeAdditionPageModel.cshtml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BackendService.Models;
+using BackendService.Models.InspectionViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendService.Pages.Images
+{
+    public class RopeAdditionPageModelModel : PageModel
+    {
+        List<RopeShipName> datalist;
+
+        public void PopulateRopesDropDownList(BackendServiceContext _context,
+            Ship selectedShip)
+        {
+            var AllShips = _context.Ship;
+            var ShipRopes = new HashSet<int>(selectedShip.Ropes.Select(r => r.RopeID));
+
+            datalist = new List<RopeShipName>();
+            foreach (var ship in AllShips)
+            {
+                foreach (var rope in ship.Ropes)
+                {
+                    datalist.Add(new RopeShipName
+                    {
+                        ShipName = ship.Name,
+                        ShipID = ship.ShipID,
+                        RopeID = rope.RopeID
+
+                    });
+                }
+            }
+
+        }
+
+        public void UpdateRopesDropDownList(BackendServiceContext context, string selectedShip, Rope ropeToUpd, Image img)
+        {
+            Ship shipToUpd = new Ship();
+            if (selectedShip == null)
+            {
+                ropeToUpd.Images = new List<Image>();
+            }
+            else
+            {
+                shipToUpd = context.Ship.Where(s => s.Name == selectedShip).Single();
+            }
+
+
+            var selShipRopes = new HashSet<int>(shipToUpd.Ropes.Select(r => r.RopeID));
+
+            foreach(var rope in context.Rope)
+            {
+                if (selectedShip == rope.Ship.Name)
+                {
+                    if (!ropeToUpd.Images.Contains(img))
+                        {
+                        ropeToUpd.Images.Add(img);
+                        //shipToUpd.Ropes.Select(r=> r.RopeID == ropeToUpd.RopeID).Single()
+                    }
+                }
+            }
+
+        }
+
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
